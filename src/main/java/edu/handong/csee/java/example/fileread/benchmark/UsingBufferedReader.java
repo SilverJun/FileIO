33c07342191cada9df8fff81be5076ab1b301a8e
SilverJun <jangej1031@naver.com>
2019-05-09 17:12:29 +0900

    Fixed UsingBufferedReader to use by args

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -7,9 +7,18 @@ import java.io.*;
  */
 public class UsingBufferedReader {
     public static void main(String[] args) throws IOException {
+    	
+    	StopWatch.start();
 
+        BufferedReader inputStream= new BufferedReader(new FileReader(args[0]));
+        while (inputStream.read()!=-1){}
 
-        //-------------- Test reading 1 MB file. --------------------
+        long duration = StopWatch.stop();
+        System.out.println(duration + " milsec");
+        
+        inputStream.close();
+
+        /*//-------------- Test reading 1 MB file. --------------------
 
         StopWatch.start();
 
@@ -33,6 +42,7 @@ public class UsingBufferedReader {
         System.out.println(duration2 + " milsec");
 
         inputStream2.close();
+        */
 
         /*
         //-------------- Test reading 100 MB file. --------------------
